3bef444a27a2d2ed5d457caf6744f2a6e227397f
Andre Sakuma <andre.sakuma@tenda.digital>
2022-01-25 20:27:56 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Layers Command Line Interface
+
+Parallel project to simplify Layers's repos first usage
+
+### Tasks
+- [-] *layers doctor*: should check if the directory is well configurated;
+  - [-] *node version step*: verify node version;
+  - [ ] *mongo step*: verify if mongoDB is running;
+  - [ ] *redis step*: verify if Redis is running;
+- [ ] *configuration file (config.layers)*: a file to specify the project's configuration;
+  - [ ] *SPECS*: specifies the technologies used in the project (<techName>:<version>);
+  - [ ] *REQUIRES*: pre conditions to run the project;
+  - [ ] *STEPS*: commands to run step-by-step (*to be discussed*);
+- [ ] *automatic installation*;
---
+++ layers.sh
@@ -0,0 +1,53 @@
+#!/bin/zsh
+
+# source layers.sh
+LAYERS_DIR=('layers-comunicados' 'layers-atendimentos' 'tendaedu-web' 'tendaedu-backend')
+
+layers-cli() {
+  echo "hello"
+}
+
+layers_dir() {
+  SRC=$PWD
+  BASE=${SRC##*/}
+
+  echo $BASE
+}
+
+layers_help() {
+  echo "HELP"
+}
+
+layers_node() {
+  NODE_VERSION="$(node --version)"
+
+  # check in nvmrc
+  if [[ -e .nvmrc ]]; then
+    DIR_VERSION="$(cat .nvmrc)"
+    SHORT="${DIR_VERSION::3}"
+    MAJOR_NODEVER="${NODE_VERSION::3}"
+    echo ${SHORT}
+    if [[ ${MAJOR_NODEVER} == ${SHORT} ]]; then
+      echo "okay"
+    else
+      echo "wrong ver"
+    fi
+  fi
+}
+
+
+main() {
+  COMMANDS=('dir' 'node' 'help')
+  echo
+  if [[ -n "$1" ]]; then
+    if [[ " ${COMMANDS[@]} " =~ " ${1} " ]]; then
+      layers_$1
+    else 
+      echo "'$1' command not found"
+    fi
+  else
+    echo "show help"
+  fi
+}
+
+main $*
